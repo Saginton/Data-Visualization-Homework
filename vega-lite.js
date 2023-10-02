3cf53b857e988ccf119b79cf35ed814328dd4b43
Saginton <70796189+Saginton@users.noreply.github.com>
2023-10-02 21:25:09 +0800

    Update vega-lite.js

--- vega-lite.js
+++ vega-lite.js
@@ -1,4 +1,4 @@
-var vis1 = "Vega-lite.json"
+var vis1 = "https://github.com/Saginton/Data-Visualization-Homework/blob/main/Vega-lite.json"
 vegaEmbed("vis", vis1).then(function (result){
 
 }).catch(console.error)
