05fdf845ab4767bfd359cdfcf30ff68d7b2e5b05
Saginton <70796189+Saginton@users.noreply.github.com>
2023-10-03 00:21:42 +0800

    Update vega-lite.js

--- vega-lite.js
+++ vega-lite.js
@@ -1,4 +1,4 @@
-var vis1 = "https://github.com/Saginton/Data-Visualization-Homework/blob/main/Vega-lite.json"
-vegaEmbed("vis", vis1).then(function (result){
-
-}).catch(console.error)
+var vg_1 = "https://github.com/Saginton/Data-Visualization-Homework/blob/main/Vega-lite.json"; 
+vegaEmbed("#vis", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
